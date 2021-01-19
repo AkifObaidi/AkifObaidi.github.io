0c6b716ba01670efa34997a6a9b28869a3ed128f
AkefObaide <sayedakefobaide@gmail.com>
2021-01-19 12:15:47 +0430

    Created Portfolio

---
+++ script/app.js
@@ -0,0 +1,10 @@
+//for scroll page by button
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", e=>{
+    window.scrollTo({
+        top:525,
+        left:0,
+        behavior:"smooth"
+    });
+});
+//script 10 line
